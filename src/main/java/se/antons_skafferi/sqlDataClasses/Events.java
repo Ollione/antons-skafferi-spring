7a1a7479ffc784e278342d92f4c0e44e21883b2a
ollione <oliverolofsson2003@icloud.com>
2024-12-08 00:30:19 +0100

    added more tables

---
+++ src/main/java/se/antons_skafferi/sqlDataClasses/Events.java
@@ -0,0 +1,46 @@
+package se.antons_skafferi.sqlDataClasses;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.sql.Date;
+import java.sql.Time;
+
+@Entity
+@Table(name = "events")
+public class Events {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer event_id;
+    private String name;
+    private String description;
+    private Date date;
+    private Time start_time;
+    private Time end_time;
+
+    public Integer getEvent_id() {
+        return event_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Time getStart_time() {
+        return start_time;
+    }
+
+    public Time getEnd_time() {
+        return end_time;
+    }
+}
---
+++ src/main/java/se/antons_skafferi/sqlDataClasses/Shift.java
@@ -0,0 +1,46 @@
+package se.antons_skafferi.sqlDataClasses;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.sql.Date;
+import java.sql.Time;
+
+@Entity
+@Table(name = "shift")
+public class Shift {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer schedule_id;
+    private Date shift_date;
+    private Time start_time;
+    private Time end_time;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    public Integer getSchedule_id() {
+        return schedule_id;
+    }
+
+    public Date getShift_date() {
+        return shift_date;
+    }
+
+    public Time getStart_time() {
+        return start_time;
+    }
+
+    public Time getEnd_time() {
+        return end_time;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+}
